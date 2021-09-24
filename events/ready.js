af93743df1e0b0a6187159f742cc0f383ae0808c
yayeen <88643636+yayeen@users.noreply.github.com>
2021-09-25 00:25:07 +0800

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-const client = require('../index')
-
-client.on('ready', () => {
-    console.log(`${client.user.tag} is online`)
-    client.user.setStatus('idle')
-    client.user.setPresence({ activities: [ {name: '+help'} ] } )
-    
-})
